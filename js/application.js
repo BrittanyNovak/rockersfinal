c5c21116e8fe7b63a34177f075a38ea75b0b5e37
BrittanyNovak <brittanynicole0106@gmail.com>
2014-08-29 00:28:42 -0500

    still trying

--- js/application.js
+++ js/application.js
@@ -10,30 +10,30 @@ function makeMessage() {
 	
 	
 	var alertMessage ="";
-	if(t1.val() == ""){ alertMessage += " Missing:Input 1, ";t1.addClass("missing"); }
+	if (t1.val() == ""){ alertMessage += " Missing:Input 1, ";t1.addClass("missing"); }
 	else { t1.removeClass("missing"); }
-	if(t2.val() == ""){ alertMessage += " Missing:Input 2, ";t2.addClass("missing"); }
+	if (t2.val() == ""){ alertMessage += " Missing:Input 2, ";t2.addClass("missing"); }
 	else { t2.removeClass("missing"); }
-	if(t3.val() == ""){ alertMessage += " Missing:Input 3, ";t3.addClass("missing"); }
+	if (t3.val() == ""){ alertMessage += " Missing:Input 3, ";t3.addClass("missing"); }
 	else { t3.removeClass("missing"); }
-	if(t4.val() == ""){ alertMessage += " Missing:Input 4, ";t4.addClass("missing"); }
+	if (t4.val() == ""){ alertMessage += " Missing:Input 4, ";t4.addClass("missing"); }
 	else { t4.removeClass("missing"); }
-	if(t5.val() == ""){ alertMessage += " Missing:Input 5, ";t5.addClass("missing"); }
+	if (t5.val() == ""){ alertMessage += " Missing:Input 5, ";t5.addClass("missing"); }
 	else { t5.removeClass("missing"); }
-	if(t6.val() == ""){ alertMessage += " Missing:Input 6, ";t6.addClass("missing"); }
+	if (t6.val() == ""){ alertMessage += " Missing:Input 6, ";t6.addClass("missing"); }
 	else { t6.removeClass("missing"); }
-	if(t7.val() == ""){ alertMessage += " Missing:Input 7, ";t7.addClass("missing"); }
+	if (t7.val() == ""){ alertMessage += " Missing:Input 7, ";t7.addClass("missing"); }
 	else { t7.removeClass("missing"); }
 	
 	if(alertMessage != ""){alert("You are missing\n" + alertMessage);}
 	else {
 	var message= "One day I will travel to " + t1.val() + "<br>";
 	message += " To become a famous " + t2.val() +  "<br>";
-	message+= " Live in a " + t3.val() + "<br>";
-	message+= " Grow a garden of " + t4.val() + "<br>";
-	message+= " Practice swimming in a pool filled with " + t5.val() + "<br>";
-	message+= " Then sing this song every night " + t6.val() + "<br>";
-	message+= " right before I " + t7.val() "<br>";
+	message += " Live in a " + t3.val() + "<br>";
+	message += " Grow a garden of " + t4.val() + "<br>";
+	message += " Practice swimming in a pool filled with " + t5.val() + "<br>";
+	message += " Then sing this song every night " + t6.val() + "<br>";
+	message += " right before I " + t7.val() + "<br>";
 	$("#welcomeMessage").append(message);
 		}
 }
